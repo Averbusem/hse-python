9ad86688b995d4201cee9a775d5e653f182f850a
averbusem <semyontechguru@gmail.com>
2024-09-21 19:49:08 +0300

    task1 of 1week has been solved (3)

---
+++ 1week/task1.py
@@ -0,0 +1,17 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1393342038/?envType=problem-list-v2&envId=string
+"""
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        left = 0
+        visited = set()
+        maxcnt = 0
+        for right in range(len(s)):
+            while s[right] in visited:
+                visited.remove(s[left])
+                left += 1
+            visited.add(s[right])
+            maxcnt = max(maxcnt, right - left + 1)
+        return maxcnt
