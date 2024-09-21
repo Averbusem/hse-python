32461647db3d7dcee8bbb82ae1c7fde39d2298aa
averbusem <semyontechguru@gmail.com>
2024-09-22 00:50:09 +0300

    task6 of 1week has been solved (71)

---
+++ 1week/task6.py
@@ -0,0 +1,28 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/simplify-path/description/?envType=problem-list-v2&envId=string&difficulty=MEDIUM
+"""
+
+
+class Solution(object):
+    def simplifyPath(self, path):
+        path_list = path.split("/")
+        # print(path_list)
+        stack = []
+        for s in path_list:
+            if s == "." or s == "":
+                continue
+            elif s == "..":
+                if stack:
+                    stack.pop()
+            # почему нельзя написать elif s == '..' and stack
+            else:
+                stack.append(s)
+        res = "/" + "/".join(stack)
+        return res
+
+
+sol = Solution()
+print(sol.simplifyPath("/.../a/../b/c/../d/./"))
+#                      /.../b/d
+print(sol.simplifyPath("/../"))
